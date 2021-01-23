420fa45ed33e86db9fdb473bb0029b2f50f2eae1
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-23 17:50:35 -0500

    Update c4N4Re_on_startup_windows.py

--- c4N4Re_on_startup_windows.py
+++ c4N4Re_on_startup_windows.py
@@ -4,7 +4,7 @@ import os
 def add_to_registry(): 
     pth = os.path.dirname(os.path.realpath(__file__))
     s_name="c4N4Re.py"
-    address=os.join(pth,s_name)  
+    address=os.path.join(pth,s_name)  
     key = HKEY_CURRENT_USER 
     key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
     open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
