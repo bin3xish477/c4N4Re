fd6d1e606ace14515ee75f8b1cd213bafb036ddc
zeugirdoR sixelA <44281620+binexisHATT@users.noreply.github.com>
2021-01-23 17:47:58 -0500

    Create c4N4Re_on_startup_windows.py

---
+++ c4N4Re_on_startup_windows.py
@@ -0,0 +1,15 @@
+import winreg as reg  
+import os              
+  
+def add_to_registry(): 
+    pth = os.path.dirname(os.path.realpath(__file__))
+    s_name="c4N4Re.py"
+    address=os.join(pth,s_name)  
+    key = HKEY_CURRENT_USER 
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
+    open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
+    reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) 
+    reg.CloseKey(open)
+    
+if __name__=="__main__": 
+    add_to_registry() 
